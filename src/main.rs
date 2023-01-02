7188357c6d56553c133e6498b672b6beb7502135
Trevor French <trevorf96@gmail.com>
2023-01-02 11:28:51 -0700

    guessing game

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,44 @@
 // cargo new [DIR_NAME]
 // cargo build
+// cargo build --release
+// cargo update (updates crates(dependencies))
+// cargo run
+// doc --open (to generate dependency docs)
+// https://doc.rust-lang.org/book/ch01-03-hello-cargo.html
+use std::io;
+use rand::Rng;
+use std::cmp::Ordering;
 
 fn main() {
-    println!("Hello, world!");
+    println!("Guess the number!");
+
+    let secret_number = rand::thread_rng().gen_range(1..=100);
+
+    // println!("The secret number is: {secret_number}");
+
+    loop {
+        println!("Please input your guess.");
+
+        let mut guess = String::new();
+
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
+
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+
+        println!("You guessed: {guess}");
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            }
+        }
+    }
 }
